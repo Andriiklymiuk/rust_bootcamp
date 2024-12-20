ba98f62c13e34b4941b008e25f1554c490f11e16
Andrii Klymiuk <ext-andrii.klymiuk@skeepers.io>
2024-12-20 12:25:10 +0200

    Use default run to run main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,3 @@
-use std::fs::File;
-use std::io::{self, BufRead, BufReader};
-use clap::Parser;
-
-#[derive(Parser)]
-struct Args {
-    #[arg(short, long)]
-    file: String,
-}
-
-fn main() -> io::Result<()> {
-    let args = Args::parse();
-    let file = File::open(args.file)?;
-    let reader = BufReader::new(file);
-
-    for line in reader.lines() {
-        let line = line?;
-        println!("{}", line);
-    }
-
-    Ok(())
+fn main() -> () {
+    println!("Hello, world!");
 }
